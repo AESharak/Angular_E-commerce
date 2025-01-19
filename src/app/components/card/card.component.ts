170b115738a247a6ab616c9902223d4f1e117b24
Ahmed Essam <ahmed.essam.sharak@gmail.com>
2025-01-19 14:24:52 +0200

    making the details of each product page

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -4,6 +4,7 @@ import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
 import { Product } from '../../models/products.interface';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -17,7 +18,17 @@ export class CardComponent {
   @Input() product!: Product;
   @Output() addToCartEvent = new EventEmitter<Product>();
 
-  addToCart() {
+  constructor(private router: Router) {}
+
+  goToDetails(event: Event) {
+    // Prevent navigation if clicking on button
+    if (!(event.target as HTMLElement).closest('button')) {
+      this.router.navigate(['/details', this.product.id]);
+    }
+  }
+
+  addToCart(event: Event) {
+    event.stopPropagation();
     if (this.product.stock > 0) {
       this.addToCartEvent.emit(this.product);
     }
